18fac49f405b91f15c87a9d71347c559f0e713da
Geomatic <73552283+SGA-A@users.noreply.github.com>
2024-01-13 14:47:50 +0000

    Moderation commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,19 +1,11 @@
-from discord import Thread
 from discord.ext import commands
+from discord import Thread
+
 
 
 class Moderation(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
-
-    @staticmethod
-    async def lock_and_close(thread: Thread, user: abc.User) -> None:
-
-        await thread.edit(
-            locked=True,
-            archived=True,
-            reason=f'Requested by {user} (ID: {user.id})'
-        )
 
     @commands.command(name="close", description="close the invocation thread.")
     @commands.guild_only()
@@ -24,70 +16,32 @@ class Moderation(commands.Cog):
 
             if permissions.manage_threads or (ctx.channel.owner_id == ctx.author.id):
 
-                await ctx.send("This thread has been auto-archived and locked due to lack of use.\n"
-                               "It may be re-opened if needed by contacting an admin.")
+                await ctx.send("<:padlocke:1195739398323581011> This thread has been auto-archived "
+                               "and locked due to lack of use.\nIt may be re-opened if needed by contacting an admin.")
                 await ctx.channel.edit(
                     locked=True,
                     archived=True,
                     reason=f'Marked as closed by {ctx.author} (ID: {ctx.author.id})'
                 )
                 return
-            return await ctx.reply("You don't have the necessary permissions.", mention_author=False)
+            return await ctx.reply(
+                "<:warning_nr:1195732155544911882> You don't have the required permissions.",
+                mention_author=False)
         else:
-            await ctx.reply("This is not a thread.", mention_author=False)
+            await ctx.reply("<:warning_nr:1195732155544911882> This is not a thread.", mention_author=False)
 
-    @commands.command(name="kick", description="kicks a user from the invocation server.")
-    @commands.has_permissions(kick_members=True)
-    @commands.guild_only()
-    async def kick(self, ctx: commands.Context, user_to_kick: Member):
-        """Kicks a member. This member must be mentioned. You must have the appropriate permissions."""
-        try:
-            await user_to_kick.kick(reason=f'Requested by {ctx.author.name}.')
-            await ctx.message.add_reaction('<:successful:1183089889269530764>')
-        except Forbidden:
-            return await ctx.send(f"You are missing permissions required to ban a user.")
-        except HTTPException:
-            return await ctx.send("Failed to kick this user.")
-
-    @commands.command(name="ban", description="bans a user from the invocation server.")
-    @commands.has_permissions(ban_members=True)
-    @commands.guild_only()
-    async def ban(self, ctx: commands.Context, user_to_ban: Member):
-        try:
-            await user_to_ban.ban(delete_message_seconds=604800, reason=f"Requested by {ctx.author.name}.")
-            await ctx.message.add_reaction('<:successful:1183089889269530764>')
-        except NotFound:
-            return await ctx.send("Could not find that user.")
-        except Forbidden:
-            return await ctx.send(f"You are missing permissions required to ban a user.")
-        except HTTPException:
-            return await ctx.send("Failed to ban this user.")
-
-    @commands.command(name='setdelay', description='sets slowmode for the invoker channel.')
+    @commands.command(name='delay', description='sets slowmode for the invoker channel.', aliases=('d',))
     @commands.has_permissions(manage_channels=True)
     async def set_delay(self, ctx: commands.Context, slowmode_in_seconds: int):
         """Sets a delay to which users can send messages. You must have the appropriate permissions."""
-
+        slowmode_in_seconds = abs(slowmode_in_seconds)
         await ctx.channel.edit(slowmode_delay=slowmode_in_seconds)
-        await ctx.message.add_reaction('<:successful:1183089889269530764>')
+        if slowmode_in_seconds:
+            return await ctx.send(f'<:slowed:1195739862100353114> Slowmode set to {slowmode_in_seconds} seconds.')
+        await ctx.send("<:normale:1195740534703136921> Disabled slowmode.")
 
-    @commands.command(name="cc", description="create a text channel to the invoker guild.")
-    @commands.has_permissions(manage_channels=True)
-    @commands.guild_only()
-    async def add_channel(self, ctx: commands.Context, channel_name: str):
-        """Creates a channel. You must have the appropriate permissions."""
-        await ctx.guild.create_text_channel(channel_name, category=ctx.channel.category)
-        await ctx.send(f"New channel created with name '{channel_name}'.")
 
-    @commands.command(name="rc", description="delete a channel from the invoker guild.")
-    @commands.has_permissions(manage_channels=True)
-    @commands.guild_only()
-    async def rc(self, ctx: commands.Context, channel_to_delete: abc.GuildChannel):
-        """Removes a channel specified. You must have the appropriate permissions."""
-        await channel_to_delete.delete(reason=f'Requested by {ctx.author.name}')
-        await ctx.send(f"Deleted channel with name {channel_to_delete.name}.")
-
-    @commands.command(name="purge", description="bulk remove an amount of messages, excluding pins.")
+    @commands.command(name="purge", description="bulk-remove messages, excluding pins.")
     @commands.has_permissions(manage_messages=True)
     @commands.guild_only()
     async def purge(self, ctx: commands.Context, purge_max_amount: int):
