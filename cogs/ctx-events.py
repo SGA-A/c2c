b45809f306c15c2035f1586e5cc1572d1c5dacd3
Geomatic <73552283+SGA-A@users.noreply.github.com>
2024-01-13 13:08:37 +0000

    Update ctx-events.py

--- cogs/ctx-events.py
+++ cogs/ctx-events.py
@@ -7,7 +7,7 @@ from discord.ext.commands import errors
 def membed(custom_description: str) -> Embed:
     """Quickly create an embed with a custom description using the preset."""
     membedder = Embed(colour=Colour.dark_embed(),
-                           description=custom_description)
+                      description=custom_description).set_thumbnail(url="https://i.imgur.com/zGtq4Dp.png")
     return membedder
 
 
@@ -21,13 +21,11 @@ class ContextCommandHandler(commands.Cog):
         if isinstance(err, errors.UserInputError):
 
             if isinstance(err, errors.MissingRequiredArgument):
-                print_exception(type(err), err, err.__traceback__)
                 await ctx.reply(
                     embed=membed(f"## A required argument is missing.\n"
                                  f"- `{err.param.name}` of type **`{err.param.kind}`**."), mention_author=False)
 
             elif isinstance(err, errors.BadArgument):
-                print_exception(type(err), err, err.__traceback__)
                 await ctx.reply(
                     embed=membed("## Bad arguments were encountered:\n"
                                  "A parsing or conversion failure has been encountered on an argument "
@@ -36,37 +34,46 @@ class ContextCommandHandler(commands.Cog):
                                  "under the limits of the API. Check what your inputs are, and try again."))
             else:
                 params = ctx.command.params
-                await ctx.reply(f"Some inputs we've received from you aren't valid forms of input.\n"
-                                f"We expect these arguments from you when calling this command: {', '.join(params.keys())}")
+                await ctx.reply(
+                    embed=membed(f"## <Invalid Input Type\n"
+                                 f"We expect these arguments from you when calling this command: {', '.join(params.keys())}"))
 
         elif isinstance(err, errors.CheckFailure):
 
             if isinstance(err, errors.NotOwner):
-                await ctx.reply(f"You are not the owner of this bot.", mention_author=False)
+                await ctx.reply(
+                    embed=membed(
+                        f"## Checks Failed\n"
+                        f"You are not the owner of this bot."), mention_author=False)
 
             elif isinstance(err, errors.MissingPermissions):
 
                 errorc = Embed(
-                    description=f"You're missing some permissions required to use this command.", colour=0x2F3136)
+                    description=f"## Missing Permissions\n"
+                                f"You're missing some permissions required to use this command.",
+                    colour=0x2F3136)
                 errorc.add_field(name='Required permissions',
                                     value=', '.join(err.missing_permissions), inline=False)
                 await ctx.reply(embed=errorc, mention_author=False)
 
             elif isinstance(err, errors.MissingRole):
 
-                exception = Embed(description=f'## You are missing a role.', colour=0x2F3136)
+                exception = Embed(description=f'## You need a required role', colour=0x2F3136)
                 exception.add_field(name='Required Role', value=f"<@&{err.missing_role}>", inline=False)
                 await ctx.reply(embed=exception, mention_author=False)
 
             elif isinstance(err, errors.NoPrivateMessage):
                 embed = membed(
-                    f"<:warningYellow:1159512635218342009> Do not send private messages to me.\n"
+                    f"## Do not send private messages.\n"
                     f"Use my commands in a server, for example in {ctx.author.mutual_guilds[0].name}.")
                 msg = await ctx.send(embed=embed)
                 await msg.delete(delay=15.0)
                 return
             else:
-                await ctx.reply(f"You have not met a prerequisite before executing this command.")
+                await ctx.reply(
+                    embed=membed(f"## Checks Failed\n"
+                                 f"You have not met a prerequisite before executing this command."),
+                mention_author=False)
 
         elif isinstance(err, errors.CommandOnCooldown):
             exception = membed(f"You're on cooldown to avoid overloading the bot.\n"
@@ -77,7 +84,9 @@ class ContextCommandHandler(commands.Cog):
             await ctx.reply(f"The command requested for was not found.", mention_author=False)
 
         else:
-            await ctx.reply(f"Something went wrong. The developers have been notified.")
+            await ctx.reply(
+                embed=membed(f"## <:alerte:1195682149253271613> Something went wrong\n"
+                             f"An error was encountered. The developers have been notified."))
             print_exception(type(err), err, err.__traceback__)
 
 
