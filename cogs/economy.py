ae2dff90386ecda07a18bda933e70c1bb5c52bd9
Geomatic <73552283+SGA-A@users.noreply.github.com>
2024-01-14 11:28:58 +0000

    Move location of ephemerality checks

--- cogs/economy.py
+++ cogs/economy.py
@@ -2146,23 +2146,23 @@ class Economy(commands.Cog):
         
         if user is None:
             user = interaction.user
-        ephemerality = False
-
-        if (get_profile_key_value(f"{user.id} vis") == "private") and (interaction.user.id != user.id):
-            return await interaction.response.send_message( 
-                embed=discord.Embed(
-                    colour=0x2F3136,
-                    description=f"# <:security:1153754206143000596> {user.name}'s profile is protected.\n"
-                                f"- Only approved users can view {user.name}'s profile."))
-
-        if (get_profile_key_value(f"{user.id} vis") == "private") and (interaction.user.id == user.id):
-            ephemerality = True
 
         async with self.client.pool_connection.acquire() as conn: 
 
             if await self.can_call_out(user, conn):
                 return await interaction.response.send_message(embed=NOT_REGISTERED) 
+                
+            ephemerality = False
+            if (get_profile_key_value(f"{user.id} vis") == "private") and (interaction.user.id != user.id):
+                return await interaction.response.send_message( 
+                    embed=discord.Embed(
+                        colour=0x2F3136,
+                        description=f"# <:security:1153754206143000596> {user.name}'s profile is protected.\n"
+                                    f"- Only approved users can view {user.name}'s profile."))
 
+            if (get_profile_key_value(f"{user.id} vis") == "private") and (interaction.user.id == user.id):
+                ephemerality = True
+            
             users = await conn.execute(f"SELECT * FROM `bank` WHERE userID = ?", (user.id,))
             users = await users.fetchone()
 
