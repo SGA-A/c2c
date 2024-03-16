bcb1fb6d55e86409728372533b3416c67ce65808
Geomatic <73552283+SGA-A@users.noreply.github.com>
2024-03-16 16:41:15 +0000

    Optimize imports

--- cogs/ctx-events.py
+++ cogs/ctx-events.py
@@ -1,14 +1,13 @@
 from traceback import print_exception
-from discord import Embed, Colour
+from datetime import timedelta
+
+from discord import Embed
 from discord.ext import commands
+from discord.utils import format_dt, utcnow
 from discord.ext.commands import errors
 
-
-def membed(custom_description: str) -> Embed:
-    """Quickly create an embed with a custom description using the preset."""
-    membedder = Embed(colour=Colour.dark_embed(), description=custom_description).set_thumbnail(
-        url="https://i.imgur.com/zGtq4Dp.png")
-    return membedder
+from cogs.economy import membed
+from cogs.slash_events import MessageDevelopers
 
 
 class ContextCommandHandler(commands.Cog):
@@ -19,65 +18,65 @@ class ContextCommandHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, err: Exception):
         """The function that handles all the errors passed into the bot via text-based commands."""
-
+        contact_view = MessageDevelopers(self.client)
         if isinstance(err, errors.UserInputError):
 
             if isinstance(err, errors.MissingRequiredArgument):
                 await ctx.reply(
-                    embed=membed(
-                        "## A required argument is missing.\n"
-                        f"- `{err.param.name}` of type **`{err.param.kind}`**."),
-                    mention_author=False)
+                    embed=membed("Some required arguments are missing."),
+                    view=contact_view)
 
-            elif isinstance(err, errors.BadArgument):
-                await ctx.reply(
-                    embed=membed(
-                        "## Bad arguments were encountered:\n"
-                        "A parsing or conversion failure has been encountered on an argument "
-                        "to pass into the command. Check what your inputs are, and try again."))
-            else:
-                params = ctx.command.params
-                await ctx.reply(
-                    embed=membed("## Invalid Input Type\n"
-                                 "We expect these arguments from you when calling this command:"
-                                 f" {', '.join(params.keys())}"))
+            await ctx.reply(
+                embed=membed("That didn't work. Check your inputs are valid."), 
+                view=contact_view)
 
         elif isinstance(err, errors.CheckFailure):
 
             if isinstance(err, errors.NotOwner):
                 await ctx.reply(
-                    embed=membed(
-                        "## Checks Failed\n"
-                        "You are not the owner of this bot."), mention_author=False)
+                    embed=membed("You do not own this bot."), 
+                    view=contact_view, 
+                    mention_author=False)
 
             elif isinstance(err, errors.MissingPermissions):
-
-                errorc = Embed(
-                    description="## Missing Permissions\n"
-                                "You're missing some permissions required to use this command.",
-                    colour=0x2F3136)
-                errorc.add_field(name='Required permissions',
-                                 value=', '.join(err.missing_permissions), inline=False)
-                await ctx.reply(embed=errorc, mention_author=False)
+                errorc = membed("You're missing some permissions required to use this command.")
+                await ctx.reply(
+                    embed=errorc, view=contact_view, mention_author=False)
 
             elif isinstance(err, errors.MissingRole):
 
-                exception = Embed(description='## You need a required role', colour=0x2F3136)
-                exception.add_field(name='Required Role', value=f"<@&{err.missing_role}>",
-                                    inline=False)
-                await ctx.reply(embed=exception, mention_author=False)
+                embed = membed(f"You're missing a required role: <@&{err.missing_role}>")
+                await ctx.reply(
+                    embed=embed, mention_author=False, view=contact_view)
 
         elif isinstance(err, errors.CommandOnCooldown):
-            exception = membed("You're on cooldown to avoid overloading the bot.\n"
-                               f"Try again after **{err.retry_after:.2f}** seconds.")
-            await ctx.reply(embed=exception)
+            after_cd = format_dt(utcnow() + timedelta(seconds=err.retry_after), style="R")
+
+            await ctx.reply(
+                embed=membed(f"You're on cooldown. Try again {after_cd}"),
+                mention_author=False,
+                view=contact_view)
 
         elif isinstance(err, errors.CommandNotFound):
-            await ctx.reply("Could not find what you were looking for.", mention_author=False)
+            await ctx.reply(
+                "Could not find what you were looking for.",
+                mention_author=False,
+                view=contact_view)
 
         else:
-            await ctx.reply("Something fucked up", mention_author=False)
             print_exception(type(err), err, err.__traceback__)
+            
+            error = Embed(colour=0x2B2D31)
+            error.title = "Something went wrong"
+            error.description = (
+                "Seems like the bot has stumbled upon an unexpected error. "
+                "Not to worry, these things happen from time to time. If this issue persists, "
+                "please let us know about it. We're always here to help!")
+            
+            await ctx.reply(
+                embed=error, 
+                mention_author=False, 
+                view=contact_view)
 
 
 async def setup(client):
