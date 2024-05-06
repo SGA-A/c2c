cc5c715eb1934cb7d8657ab4f958840bb7345c95
Geomatic <73552283+SGA-A@users.noreply.github.com>
2024-05-06 15:03:11 +0100

    Remove allowed_mentions kwarg
    Replaced for client initialized allowed mentions kwarg.

--- cogs/ctx_events.py
+++ cogs/ctx_events.py
@@ -19,66 +19,42 @@ class ContextCommandHandler(commands.Cog):
     async def on_command_error(self, ctx: commands.Context, err: Exception):
         """The function that handles all the errors passed into the bot via text-based commands."""
         contact_view = MessageDevelopers()
-        
+        embed = membed()
+
         if isinstance(err, commands.errors.UserInputError):
 
             if isinstance(err, self.err.MissingRequiredArgument):
-                return await ctx.reply(
-                    embed=membed("Some required arguments are missing."),
-                    mention_author=False,
-                    view=contact_view
-                )
-
-            await ctx.reply(
-                embed=membed("That didn't work. Check your inputs are valid."), 
-                mention_author=False,
-                view=contact_view
-            )
+                embed.description = "Some required arguments are missing."
+                return await ctx.reply(embed=embed, view=contact_view)
+            
+            embed.description = "That didn't work. Check your inputs are valid."
+            await ctx.reply(embed=embed, view=contact_view)
 
         elif isinstance(err, self.err.CheckFailure):
 
             if isinstance(err, self.err.NotOwner):
-                await ctx.reply(
-                    embed=membed("You do not own this bot."), 
-                    view=contact_view, 
-                    mention_author=False
-                )
+                embed.description = "You do not own this bot."
+                await ctx.reply(embed=embed, view=contact_view)
 
             elif isinstance(err, self.err.MissingPermissions):
-                errorc = membed("You're missing some permissions required to use this command.")
-                await ctx.reply(
-                    embed=errorc, 
-                    view=contact_view, 
-                    mention_author=False
+                embed.description = f"You're missing {len(err.missing_permissions)} permission(s) required to use this command."
+                embed.add_field(
+                    name="Missing Permissions", 
+                    value="\n".join(err.title() for err in err.missing_permissions)
                 )
+                await ctx.reply(embed=embed, view=contact_view)
 
             elif isinstance(err, self.err.MissingRole):
-
-                embed = membed(f"You're missing a required role: <@&{err.missing_role}>")
-                await ctx.reply(
-                    embed=embed, 
-                    mention_author=False, 
-                    view=contact_view
-                )
+                embed = membed("You're missing a role required to use this command.")
+                embed.add_field(name="Missing Role", value=f"<@&{err.missing_role}>")
+                await ctx.reply(embed=embed, view=contact_view)
 
         elif isinstance(err, self.err.CommandOnCooldown):
-            after_cd = format_dt(
-                utcnow() + timedelta(seconds=err.retry_after), 
-                style="R"
-            )
-
-            await ctx.reply(
-                embed=membed(f"You're on a cooldown. Try again {after_cd}."),
-                mention_author=False,
-                view=contact_view
-            )
+            after_cd = format_dt(utcnow() + timedelta(seconds=err.retry_after), style="R")
+            await ctx.reply(embed=membed(f"You're on a cooldown. Try again {after_cd}."), view=contact_view)
 
         elif isinstance(err, self.err.CommandNotFound):
-            await ctx.reply(
-                embed=membed("Could not find what you were looking for."),
-                mention_author=False,
-                view=contact_view
-            )
+            await ctx.reply(embed=membed("Could not find what you were looking for."), view=contact_view)
 
         else:
             print_exception(type(err), err, err.__traceback__)
@@ -91,11 +67,7 @@ class ContextCommandHandler(commands.Cog):
                 "please let us know about it. We're always here to help!"
             )
             
-            await ctx.reply(
-                embed=error, 
-                mention_author=False, 
-                view=contact_view
-            )
+            await ctx.reply(embed=error, view=contact_view)
 
 
 async def setup(bot: commands.Bot):
