ced26acddd505cf475dcddac83e64e822c1f4bb8
SGA-A <73552283+SGA-A@users.noreply.github.com>
2024-08-28 09:44:52 +0100

    Handle custom errors for prefixes

--- cogs/context_exceptions.py
+++ cogs/context_exceptions.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 from .core.helpers import membed, MessageDevelopers
 from .core.bot import C2C
+from .core.errors import FailingConditionalError
 
 
 class ContextExceptions(commands.Cog):
@@ -16,38 +17,35 @@ class ContextExceptions(commands.Cog):
         """The function that handles all the errors passed into the bot via text-based commands."""
 
         err = getattr(err, "original", err)
+
         if isinstance(err, commands.CommandNotFound):
             return
 
         embed = membed()
-        if isinstance(err, commands.UserInputError):
+        if isinstance(err, FailingConditionalError):
+            embed.description = err.cause
+        elif isinstance(err, commands.UserInputError):
             if isinstance(err, commands.MissingRequiredArgument):
                 embed.description = "Some required arguments are missing."
-                return await ctx.send(embed=embed, view=self.view)
-
             embed.description = "That didn't work. Check your inputs are valid."
-            return await ctx.send(embed=embed, view=self.view)
-
-        if isinstance(err, commands.CheckFailure):
-            if isinstance(err, commands.MissingPermissions):
-                embed.description = (
-                    "You're missing permissions required to use this command."
+        elif isinstance(err, commands.CheckFailure):
+            if not isinstance(err, commands.MissingPermissions):
+                return
+            embed.description = (
+                "You're missing permissions required to use this command."
+            ).add_field(
+                name=f"Missing Permissions ({len(err.missing_permissions)})",
+                value="\n".join(
+                    err.replace("_", " ").title() for err in err.missing_permissions
                 )
-                embed.add_field(
-                    name=f"Missing Permissions ({len(err.missing_permissions)})",
-                    value="\n".join(
-                        err.replace("_", " ").title() for err in err.missing_permissions
-                    )
-                )
-                return await ctx.send(embed=embed, view=self.view)
-            return
-
-        embed.title = "Something went wrong"
-        embed.description = (
-            "Seems like the bot has stumbled upon an unexpected error. "
-            "Not to worry, these things happen from time to time. If this issue persists, "
-            "please let us know about it. We're always here to help!"
-        )
+            )
+        else:
+            embed.title = "Something went wrong"
+            embed.description = (
+                "Seems like the bot has stumbled upon an unexpected error. "
+                "Not to worry, these things happen from time to time. If this issue persists, "
+                "please let us know about it. We're always here to help!"
+            )
         await ctx.send(embed=embed, view=self.view)
         self.bot.log_exception(err)
 
