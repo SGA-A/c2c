6419aa9dd79212f665ce81eda80b2634b6c4c2a0
SGA-A <73552283+SGA-A@users.noreply.github.com>
2024-09-20 23:12:49 +0100

    Add developer tools command
    Manage the bot all in one place

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,11 +7,139 @@ from typing import Any, Optional
 import asyncio
 import discord
 
+from discord import app_commands
 from discord.ext import commands
 
 from .core.bot import C2C
-from .core.helpers import membed
-from .core.constants import CURRENCY
+from .core.helpers import membed, edit_response
+
+
+class DevTools(discord.ui.View):
+    _last_result: Optional[Any] = None
+    on_boarding = dedent(
+        """
+        ## Developer Panel
+        Select the below options to get started:
+        **Quit**
+        -# Shut down the bot process gracefully.
+        **Sync**
+        -# Send a new copy of your command tree to Discord.
+        **Evaluate**
+        -# Execute arbitrary Python code within Discord.
+        **Blanket**
+        -# Heavily nest the invocation channel with new lines.
+        """
+    )
+
+    def __init__(self, interaction: discord.Interaction):
+        self.interaction = interaction
+        super().__init__(timeout=90.0)
+    
+    async def respond_timeout(self, interaction: discord.Interaction) -> None:
+        self.stop()
+        for item in self.children:
+            item.disabled = True
+        await edit_response(interaction, view=self)
+
+    @staticmethod
+    def cleanup_code(content: str) -> str:
+        # remove ```py\n```
+        if content.startswith("```") and content.endswith("```"):
+            return "\n".join(content.split("\n")[1:-1])
+
+        # remove `foo`
+        return content.strip("` \n")
+
+    @staticmethod
+    async def evaluate(interaction: discord.Interaction, message: discord.Message) -> Optional[discord.Message]:
+        env = {
+            "bot": interaction.client,
+            "itx": interaction,
+            "channel": interaction.channel,
+            "user": interaction.user,
+            "guild": interaction.guild,
+            "message": message,
+            "_": DevTools._last_result,
+            "discord": discord,
+            "asyncio": asyncio,
+        }
+
+        env.update(globals())
+
+        script_body = DevTools.cleanup_code(message.content)
+        stdout = StringIO()
+
+        to_compile = f'async def func():\n{indent(script_body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await message.reply(f"```py\n{e.__class__.__name__}: {e}\n```")
+
+        func = env["func"]
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()  # return value
+        except Exception:
+            value = stdout.getvalue()
+            await message.reply(f"```py\n{value}{format_exc()}\n```")
+        else:
+            value = stdout.getvalue()
+            try:
+                await message.add_reaction("\U00002705")
+            except discord.HTTPException:
+                pass
+
+            if ret is None:
+                if value:
+                    try:
+                        await message.reply(f"```py\n{value}\n```")
+                    except discord.HTTPException:
+                        return await message.reply("Output too long to display.")
+            else:
+                DevTools._last_result = ret
+                await message.reply(f"```py\n{value}{ret}\n```")
+
+    def eval_check(self, msg: discord.Message):
+        return (
+            (msg.channel.id == self.interaction.channel.id) and
+            (msg.author.id == self.interaction.user.id)
+        )
+
+    @discord.ui.button(label="Quit", style=discord.ButtonStyle.success)
+    async def quit_button(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        await self.interaction.delete_original_response()
+        await interaction.response.send_message("\U00002705", ephemeral=True)
+        await interaction.client.close()
+    
+    @discord.ui.button(label="Sync", style=discord.ButtonStyle.success)
+    async def sync_button(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        await interaction.client.tree.sync(guild=None)
+        await interaction.response.send_message("\U00002705", ephemeral=True)
+    
+    @discord.ui.button(label="Evaluate", style=discord.ButtonStyle.success)
+    async def eval_button(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        await interaction.response.send_message("What would you like to evaluate?")
+
+        try:
+            msg = await interaction.client.wait_for('message', check=self.eval_check, timeout=90.0)
+        except asyncio.TimeoutError:
+            await interaction.edit_original_response(content="Timed out waiting for a response.")
+        else:
+            await self.evaluate(interaction, msg)
+
+    @discord.ui.button(label="Blanket", style=discord.ButtonStyle.success)
+    async def blanket_button(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        await interaction.channel.send(f".{'\n'*1900}-# Blanked out the channel.")
+        await interaction.response.send_message(self.on_boarding, view=self, ephemeral=True)
+        await self.interaction.delete_original_response()
+
+        # Disable view items from the new message
+        self.interaction = interaction
+
+    @discord.ui.button(emoji="<:terminatePages:1263923664433319957>")
+    async def terminate_button(self, interaction: discord.Interaction, _: discord.ui.Button) -> None:
+        await self.respond_timeout(interaction)
 
 
 class Admin(commands.Cog):
@@ -19,10 +147,6 @@ class Admin(commands.Cog):
 
     def __init__(self, bot: C2C):
         self.bot = bot
-        self._last_result: Optional[Any] = None
-
-    async def cog_check(self, ctx: commands.Context) -> bool:
-        return ctx.author.id in self.bot.owner_ids
 
     async def interaction_check(self, interaction: discord.Interaction[C2C]):
         if interaction.user.id in self.bot.owner_ids:
@@ -31,109 +155,11 @@ class Admin(commands.Cog):
             embed=membed("You do not own this bot.")
         )
         return False
-
-    @staticmethod
-    def cleanup_code(content: str) -> str:
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith("```") and content.endswith("```"):
-            return "\n".join(content.split("\n")[1:-1])
-
-        # remove `foo`
-        return content.strip("` \n")
-
-    @commands.command(description="Adjust a user's robux directly")
-    async def config(
-        self,
-        ctx: commands.Context,
-        configuration: str,
-        amount: int,
-        user: discord.User | discord.Member = commands.Author,
-        medium: str = "wallet"
-    ) -> None:
-        if configuration.startswith("a"):
-            output = f"Added {CURRENCY} **{amount:,}** to {user.name}!"
-            query = f"UPDATE accounts SET {medium} = {medium} + ? WHERE userID = ?"
-
-        elif configuration.startswith("r"):
-            output = f"Deducted {CURRENCY} **{amount:,}** from {user.name}!"
-            query = f"UPDATE accounts SET {medium} = {medium} - ? WHERE userID = ?"
-
-        else:
-            output = f"Set {CURRENCY} **{amount:,}** to {user.name}!"
-            query = f"UPDATE accounts SET {medium} = ? WHERE userID = ?"
-
-        async with self.bot.pool.acquire() as conn, conn.transaction():
-            await conn.execute(query, (amount, user.id))
-
-        await ctx.send(output)
-
-    @commands.command(description="Sync the bot tree for changes", aliases=("sy",))
-    async def sync(self, ctx: commands.Context) -> None:
-        """Sync the bot's tree globally."""
-        await self.bot.tree.sync(guild=None)
-        await ctx.send("\U00002705")
-
-    @commands.command(name="eval", description="Evaluates arbitrary code")
-    async def evaluate(
-        self, ctx: commands.Context, *, script_body: str
-    ) -> Optional[discord.Message]:
-        """Evaluates arbitrary code."""
-
-        env = {
-            "bot": self.bot,
-            "ctx": ctx,
-            "channel": ctx.channel,
-            "author": ctx.author,
-            "guild": ctx.guild,
-            "message": ctx.message,
-            "_": self._last_result,
-            "discord": discord,
-            "asyncio": asyncio,
-        }
-
-        env.update(globals())
-
-        script_body = self.cleanup_code(script_body)
-        stdout = StringIO()
-
-        to_compile = f'async def func():\n{indent(script_body, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f"```py\n{e.__class__.__name__}: {e}\n```")
-
-        func = env["func"]
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()  # return value
-        except Exception:
-            value = stdout.getvalue()
-            await ctx.send(f"```py\n{value}{format_exc()}\n```")
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction("\U00002705")
-            except discord.HTTPException:
-                pass
-
-            if ret is None:
-                if value:
-                    try:
-                        await ctx.send(f"```py\n{value}\n```")
-                    except discord.HTTPException:
-                        return await ctx.send(
-                            embed=membed("Output too long to display.")
-                        )
-            else:
-                self._last_result = ret
-                await ctx.send(f"```py\n{value}{ret}\n```")
-
-    @commands.command(description="Sends newlines to clear a channel", aliases=("b",))
-    async def blank(self, ctx: commands.Context):
-        """Clear out the channel."""
-        await ctx.send(f".{'\n'*1900}-# Blanked out the channel.")
+    
+    @app_commands.command(description="Manage the bot")
+    async def devtools(self, interaction: discord.Interaction):
+        view = DevTools(interaction)
+        await interaction.response.send_message(view.on_boarding, view=view, ephemeral=True)
 
     async def send_role_guide(self):
         channel = self.bot.get_partial_messageable(1254883155882672249)
@@ -161,12 +187,6 @@ class Admin(commands.Cog):
         )
 
         await original.edit(content=dedent(role_content_pt1))
-
-    @commands.command(name="quit", description="Quits the bot gracefully", aliases=("q",))
-    async def close(self, ctx: commands.Context) -> None:
-        """Quits the bot gracefully."""
-        await ctx.send("\U00002705")
-        await self.bot.close()
 
 
 async def setup(bot: C2C):
